3b7cb722339a521bdf3853c84de4fdd98d7049ce
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-08-07 12:00:11 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
@@ -1,9 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 a, b = map(str, input().split())
+a = int(a[::-1])
+b = int(b[::-1])
+print(int(str(a+b)[::-1]))
 
-def rev(x):
-    rev_x=''
-    for i in range(len(x)-1, -1, -1):
-        rev_x += x[i]
-    return int(rev_x)
 
-print(rev(str(rev(a) + rev(b))))
+
+# 함수 이용
+# a, b = map(str, input().split())
+#
+# def rev(x):
+#     rev_x=''
+#     for i in range(len(x)-1, -1, -1):
+#         rev_x += x[i]
+#     return int(rev_x)
+#
+# print(rev(str(rev(a) + rev(b))))
