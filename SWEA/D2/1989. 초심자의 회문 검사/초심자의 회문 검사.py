5826913032265352322691e024beb3142e4e84ea
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-08-07 16:01:27 +0900

    [D2] Title: 초심자의 회문 검사, Time: 171 ms, Memory: 58,268 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -4,11 +4,14 @@ for tc in range(1, T+1):
     word = input()
     # x = word
     N=len(word)
+    # 만약 문자열이 다른 게 있다면 1씩 증가시킬 변수
     cnt = 0
+    # word 길이의 절반만큼 반복
     for i in range(len(word)//2):
+        # 만약 다르다면 +1씩
         if word[i] != word[N-1-i]:
             cnt += 1
-
+    # 모두 같다면 결과값은 1
     if cnt==0:
         result =1
     else:
