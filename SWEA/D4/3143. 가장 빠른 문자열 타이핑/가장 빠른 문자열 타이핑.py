dbb845fcef49b3cc0750ddefaf6476e274eebfd0
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-08-08 16:12:29 +0900

    [D4] Title: 가장 빠른 문자열 타이핑, Time: 353 ms, Memory: 64,896 KB -BaekjoonHub

--- SWEA/D4/3143. 가장 빠른 문자열 타이핑/가장 빠른 문자열 타이핑.py
+++ SWEA/D4/3143. 가장 빠른 문자열 타이핑/가장 빠른 문자열 타이핑.py
@@ -7,7 +7,10 @@ T = int(input())
 
 for tc in range(1, T+1):
     A, B = map(str, input().split())
+    # 몇 번 겹치는 지 탐색
     cnt = 0
+    # A의 길이만큼 새로운 리스트를 만들어서 만약 겹친다면 1의 값을 넣고
+    # 1의 값이 존재한다면(값이 0이 아니라면) 탐색을 건너 뜀
     new_list = [0] *len(A)
     for i in range(len(A)-len(B)+1):
         if new_list[i] == 0:
