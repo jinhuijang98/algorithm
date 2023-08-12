7f12be4a18ac6c9ae962a8b8f3a463a008b652e7
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-08-12 21:58:15 +0900

    [D2] Title: 풍선팡2, Time: 165 ms, Memory: 59,816 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    # N * M 행렬 만들기
+    arr = [[0]*M for _ in range(N)]
+    for i in range(N):
+        arr[i] = list(map(int, input().split()))
+    # 상, 하, 좌, 우로 움직이는 델타
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    # 그 값만큼 추가로 터짐
+    # 최대값 초기화 해놓고 시작
+    max_v = 0
+    for i in range(N):
+        for j in range(M):
+            x = arr[i][j]
+            # i, j에서의 터지는 꽃가루의 개수
+            rlt = arr[i][j]
+            # for k in range(x):
+            for l in range(4):
+                if 0 <= (i+(dx[l])) <= N-1 and 0 <= (j+(dy[l])) <= M-1:
+                    rlt += arr[i+(dx[l])][j+(dy[l])]
+            if max_v < rlt:
+                max_v = rlt
+    print(f'#{tc} {max_v}')
