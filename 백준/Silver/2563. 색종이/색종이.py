b2c3959bd1a962a31fbf734dee8836d4756b6dbe
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-08-24 19:22:38 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,24 +1,17 @@
-T = int(input())
-# from pprint import pprint
-# 초기값으로 0을 100x100 넣은 arr 만듦
+import sys
+input = sys.stdin.readline
+
+N = int(input())
 arr = [[0]*100 for _ in range(100)]
-# pprint(arr)
+cnt = 0
+for i in range(N):
+    x, y = map(int, input().split())
+    for j in range(x, x+10):
+        for k in range(y, y+10):
+            arr[j][k] += 1
 
-for tc in range(1, T+1):
-    # N과 M 값을 가져옴
-    N, M = map(int, input().split())
-    # 기존의 넓이는 100*N
-    # 기존의 넓이에서 겹치는 영역의 길이 재서 빼기
-    for i in range(10):
-        for j in range(10):
-            arr[N+i][M+j] += 1
-    # 넓이
-    con = 0
-    for i in range(100):
-        for j in range(100):
-            if arr[i][j] >= 1:
-              con += 1
-
-
-
-print(con)
+for j in range(100):
+    for k in range(100):
+        if arr[j][k] >= 1:
+            cnt += 1
+print(cnt)
