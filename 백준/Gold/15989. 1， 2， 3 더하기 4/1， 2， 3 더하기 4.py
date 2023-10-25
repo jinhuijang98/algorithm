6d4d65c1f02a718f6db7631c278cdd8a7cc7225f
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-10-25 23:14:33 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,16 @@
+# 15989 1, 2, 3 더하기 4
+# 다시
+
+t = int(input())
+
+dp = [1] * 10001
+
+for i in range(2, 10001):
+    dp[i] += dp[i - 2]
+
+for i in range(3, 10001):
+    dp[i] += dp[i - 3]
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
