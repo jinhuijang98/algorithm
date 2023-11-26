daf69e9bad8ef43b72f55463779648da85eef1da
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2023-11-26 20:48:08 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 5820 ms, Memory: 185112 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    lst = list(map(int, input().split()))
+    ans = 0
+    q = []
+    for i in lst:
+        heapq.heappush(q, i)
+    while len(q) > 1:
+        a = heapq.heappop(q)
+        b = heapq.heappop(q)
+        ans += a + b
+        heapq.heappush(q, a + b)
+    print(ans)
