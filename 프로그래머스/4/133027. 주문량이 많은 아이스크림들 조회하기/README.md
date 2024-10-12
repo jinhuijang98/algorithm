b48bd012f15616c84483e8464ddd019291fa3770
jinhuijang98 <139421078+jinhuijang98@users.noreply.github.com>
2024-10-12 21:27:23 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/README.md
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 주문량이 많은 아이스크림들 조회하기 - 133027 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133027#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133027) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 11월 2일 16:28:24
+2024년 10월 12일 21:27:17
 
 ### 문제 설명
 
--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,8 +1,7 @@
--- 코드를 입력하세요
-select fh.flavor as flavor
-from first_half as fh
-inner join july as j
-on fh.flavor = j.flavor
-group by flavor
-order by sum(fh.total_order) + sum(j.total_order) desc
-limit 3
+# select FIRST_HALF.FLAVOR
+SELECT FIRST_HALF.FLAVOR
+from FIRST_HALF JOIN JULY
+on FIRST_HALF.FLAVOR = JULY.FLAVOR
+GROUP BY FLAVOR
+ORDER BY SUM(FIRST_HALF.TOTAL_ORDER) + SUM(JULY.TOTAL_ORDER) DESC
+LIMIT 3
